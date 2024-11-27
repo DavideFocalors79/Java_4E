0c56e7eebbdfdf2588eb4b9eb5cb2822ae5da90d
DavideLaghi <150442316+DavideFocalors79@users.noreply.github.com>
2024-11-27 13:45:48 +0300

    Add files via upload

---
+++ Laghi_4E_Es7_Rettangolo/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /* istanzio tutti gli oggetti */
+        Punto puntoA = new Punto();
+        Punto puntoC = new Punto();
+        Rettangolo rettangolo = null;
+
+        /* istanzio gli oggetti per l'esecuzione del codice,
+        * gestendo eventuali eccezioni */
+        try {
+            puntoA = new Punto(2,3);
+            puntoC = new Punto(-2,7);
+            rettangolo = new Rettangolo(puntoA, puntoC);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        /* ora stampo i quadranti in cui si trovano i punti
+        * indicati */
+        try{
+            System.out.println("Il punto A si trova nel "+puntoA.getQuadrante()+"^ quadrante");
+            System.out.println("Il punto C si trova nel "+puntoC.getQuadrante()+"^ quadrante");
+            System.out.println(rettangolo.toString());
+            rettangolo.stampaRettangolo();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        /*ora traslo il rettangolo
+        *e stampo a video*/
+        rettangolo.traslaRettangolo(4,5);
+
+        try{
+            System.out.println("Il punto A si trova nel "+puntoA.getQuadrante()+"^ quadrante");
+            System.out.println("Il punto C si trova nel "+puntoC.getQuadrante()+"^ quadrante");
+            System.out.println(rettangolo.toString());
+            rettangolo.stampaRettangolo();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
